d50b6cfd50c28ec02c75814849c9969888751cd3
Martinimbastar1 <martin.imbastari98@gmail.com>
2023-11-22 20:23:20 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+console.log({ http })
+
+function requestcontroler () {
+    //logica de nuestra función
+    console.log("hola mundo" )
+}
+
+const server = http.createServer(requestcontroler)
+
+server.listen(4000)
