85ae8696e715a82308f5880a6fa165569d0e5e80
Martinimbastar1 <martin.imbastari98@gmail.com>
2023-11-25 13:11:45 -0300

    commit desde ezequiel imbastari 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http")
 const another = require('./another.js')
 
-// console.log({ http })
+console.log({ http })
 
 function requestcontroler () {
     //logica de nuestra función
-    // console.log("hola mundo!!!!" )
+    console.log("hola mundo!!!!" )
 }
 
 const server = http.createServer(requestcontroler)
