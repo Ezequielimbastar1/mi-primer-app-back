1f17e4e8e1ecf0636c13fb7fdda829926126230d
Martinimbastar1 <martin.imbastari98@gmail.com>
2023-11-25 13:38:13 -0300

    replace git ignore

---
+++ another.js
@@ -0,0 +1,9 @@
+const name = 'martin';
+
+// console.log({ name })
+module.exports = { name };
+
+// const person = {name:"martin",edad:12,}
+
+
+// console.log person.name, 'tiene',person.edad, 'años' chiky
